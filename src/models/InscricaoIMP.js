086dfd7cda4a85f908ab49026c61da9bc2f29ab2
Áquila Oliveira <oaquila14@gmail.com>
2022-08-29 02:08:02 -0300

    chore: colocando add, remove e notifyObservers em InscricaoIMP

--- src/models/InscricaoIMP.js
+++ src/models/InscricaoIMP.js
@@ -29,11 +29,19 @@ class InscricaoIMP{
     }
 
     addObserver(observer){
-
+        this.observers.push(observer)
     }
 
-    removeObserver(){
-        
+    removeObserver(observer){
+        let index = this.observers.indexOf(observer)
+        if(index >-1){
+            this.observers.slice(index,1)
+        }
+    }
+
+    notifyAllObservers(){
+        for(let o of this.observers)
+            console.log(o.name, "foi notificado")
     }
 
   }
