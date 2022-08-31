e756ee0376578decb92f777089e3bbeb403cd050
Áquila Oliveira <oaquila14@gmail.com>
2022-08-31 15:19:40 -0300

    fix: Arrumando script para o heroku

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
-const connection = require('./database/connection')
 const app = express();
 
 app.use(express.json())
 app.use(routes)
 
 
-
-app.listen(3333);
+const port = process.env.PORT || 3333
+app.listen(port, ()=>console.log(`Listening on port ${port}...`));
