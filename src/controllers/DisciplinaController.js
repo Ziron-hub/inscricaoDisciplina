00115a3edafba2056e10f3ab71081125e77930f3
Warteruzannan S. Cunha <warteruzannan@gmail.com>
2022-09-12 20:47:30 -0300

    feat: add feat propose

--- src/controllers/DisciplinaController.js
+++ src/controllers/DisciplinaController.js
@@ -1,19 +1,22 @@
-const Retangulo = require('../models/disciplinaModel') 
-const Usuario = require('../models/Usuario') 
-connection = require('../database/connection')
+const Disciplina = require("../models/Disciplina");
+const { constants } = require("http2");
+
+connection = require("../database/connection");
 
 module.exports = {
-    async create(request, response){
-        const {ano, creditos, maxCredito, semestre} = request.body
-        id = "1"
-        await connection('periodo').insert({
-            id,
-            ano,
-            creditos,
-            maxCredito,
-            semestre,
-
-        })
-        return response.json({"teste":"deu"})
+  async create(request, response) {
+    const { body: data } = request;
+    try {
+      const disciplina = await Disciplina.create(data);
+      return response.status(constants.HTTP_STATUS_CREATED).json({
+        message: "Disciplina criada com sucesso",
+        body: disciplina,
+      });
+    } catch (error) {
+      console.log({ message: "Não foi possível inserir a disciplina", error });
+      return response
+        .status(constants.HTTP_STATUS_BAD_REQUEST)
+        .json({ message: "Não foi possíveis criar a disciplina" });
     }
-}
+  },
+};
--- src/models/Disciplina.js
+++ src/models/Disciplina.js
@@ -1,51 +1,78 @@
-const Curso = module.require('./curso')
+const connection = require("../database/connection");
 
 class Disciplina {
-    #nome;
-    #codigo;
-    #numero_vagas;
-    #curso;
-    #perfil;
-    constructor(nome, codigo, numero_vagas, curso ,perfil) {
-        this.#nome = nome;
-        this.#codigo = codigo;
-        this.#numero_vagas = numero_vagas;
-        this.#curso = curso;
-        this.#perfil = perfil;
-    }
-    get nome() {
-        return this.#nome;
-    }
-    get codigo() {
-        return this.#codigo;
-    }
-    get numero_vagas() {
-        return this.#numero_vagas;
-    }
-    get curso() {
-        return this.#curso;
-    }
-    get perfil() {
-        return this.#perfil;
-    }
-    set nome(nome) {
-        this.#nome = nome;
-    }
-    set codigo(codigo) {
-        this.#codigo = codigo;
-    }
-    set numero_vagas(numero_vagas) {
-        this.#numero_vagas = numero_vagas;
-    }
-    set curso(curso) {
-        this.#curso = curso;
-    }
-    set perfil(perfil) {
-        this.#perfil = perfil;
-    }
+  #nome;
+  #codigo;
+  #numero_vagas;
+  #curso;
+  #perfil;
+  constructor({ nome, codigo, numero_vagas, curso, perfil }) {
+    this.#nome = nome;
+    this.#codigo = codigo;
+    this.#numero_vagas = numero_vagas;
+    this.#curso = curso;
+    this.#perfil = perfil;
+  }
+  get nome() {
+    return this.#nome;
+  }
+  get codigo() {
+    return this.#codigo;
+  }
+  get numero_vagas() {
+    return this.#numero_vagas;
+  }
+  get curso() {
+    return this.#curso;
+  }
+  get perfil() {
+    return this.#perfil;
+  }
+  set nome(nome) {
+    this.#nome = nome;
+  }
+  set codigo(codigo) {
+    this.#codigo = codigo;
+  }
+  set numero_vagas(numero_vagas) {
+    this.#numero_vagas = numero_vagas;
+  }
+  set curso(curso) {
+    this.#curso = curso;
+  }
+  set perfil(perfil) {
+    this.#perfil = perfil;
+  }
 
+  static async create(data) {
+    const disciplina = new Disciplina(data);
+    Disciplina.#validate(disciplina);
 
+    const insertedDisciplina = await Disciplina.#insert(disciplina);
 
+    return insertedDisciplina;
+  }
+
+  /**
+   * Valida uma disciplina
+   * @param {Disciplina} disciplina
+   */
+  static #validate() {
+    // Validações
+    if (!disciplina.nome) {
+      throw new Error("Nome inválido");
+    }
+    // outras validações
+  }
+
+  /**
+   * Insere uma disciplina no banco de dados
+   * @param {Disciplina} disciplina
+   */
+  static async #insert(disciplina) {
+    const resultSet = await connection("disciplina").insert(disciplina);
+    return resultSet.raw;
+  }
 }
 
 module.exports = Disciplina;
